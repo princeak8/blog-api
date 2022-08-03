362f0f6eab0aa93b3c9aa93b81461a9bfee87791
princeak <akalodave@gmail.com>
2022-08-03 17:03:24 +0100

    latest changes

--- resources/views/mails/test.blade.php
+++ resources/views/mails/test.blade.php
@@ -1,2 +1,22 @@
-<h1>Hi, {{ $name }}</h1>
-l<p>Sending Mail from Laravel.</p>
+<html>
+    <header>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" 
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"
+    >
+    </header>
+    <body>
+        <div style="display:flex; justify-content:center">
+            <div>
+                <div style="margin-bottom: 20px;">
+                    <img src="{{env('APP_URL').'images/car.jpg'}}" width="300" height="150" />
+                </div>
+                <div style="display:flex; flex-direction:column; align-items:center">
+                    <h1>PRINCEAK BLOG</h1>
+
+                    <p>Hi <b>{{$name}}</b>
+                    Sending Mail from Laravel.</p>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
