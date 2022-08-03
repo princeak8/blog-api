6233abb1e44e70a5703de65df35b66ecdc510625
princeak <akalodave@gmail.com>
2022-08-03 23:13:22 +0100

    changes

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -16,7 +16,7 @@ class LoginController extends Controller
      *
      * @return \Illuminate\Http\JsonResponse
      */
-    public function login(Request $request){
+    public function login(LoginRequest $request){
         dd($request->all());
         $credentials = $request->only('email', 'password');
         if (! $token = Auth::guard('reader')->attempt($credentials)) {
