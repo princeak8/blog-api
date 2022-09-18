4b2344c9a353ca668d95c4d66abbf1977572847a
princeak <akalodave@gmail.com>
2022-09-18 15:03:49 +0100

    latest changes

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -34,7 +34,7 @@ class PostController extends Controller
     {
         try{
             //dd($page);
-            if(is_int((int)$page)) {
+            if ( strval($page) == strval(intval($page)) ) {
                 //dd($page);
                 $posts = $this->postService->paginatedPosts($page);
                 $count = $this->postService->postsCount();
